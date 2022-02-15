155684a62c24a8fc2e2f50adb4015d96a03d83f5
Zhen Luo <luo.zhe@northeastern.edu>
2022-02-15 16:14:10 -0500

    Demo

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=y#@6uz*hk)-ex4vx*(_yz=4j)!u_8^t!ydh0kzn680&*@mvq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
