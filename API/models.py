3f33e7aa056ae95e66a1d51cadee385fbd7e16d5
Zhen Luo <luo.zhe@northeastern.edu>
2022-02-16 13:46:38 -0500

    function development

--- API/models.py
+++ API/models.py
@@ -1,3 +1,16 @@
+from pyexpat import model
+from turtle import update
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractUser
+)
 
 # Create your models here.
+class MyUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    account_updated = models.DateTimeField(auto_now=True)
+    account_created = models.DateTimeField(auto_now_add=True)
+    
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
