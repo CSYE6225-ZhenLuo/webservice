3f256464203b175bbfc797ed6dc18f0ce231c00a
Zhen Luo <90990036+ZhenLuoLF@users.noreply.github.com>
2022-04-01 16:00:57 -0400

    Update afterInstall.sh

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/website/website
-rm wsgi.py
-cd /home
+sudo rm -rf wsgi.py
+cd /home/ec2-user
 cp wsgi.py /var/www/website/website
 cd /var/www
 sudo chown -R apache.apache website
