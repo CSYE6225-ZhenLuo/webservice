a2cb71aa8255d57cbec40b0e85a62d34e7065931
Zhen Luo <luo.zhe@northeastern.edu>
2022-04-12 00:33:40 +0000

    a09.01

--- website/API/views.py
+++ website/API/views.py
@@ -1,5 +1,8 @@
 from datetime import date
 import datetime
+import email
+from email.message import Message
+import time
 from API.models import MyUser, UserPicture
 from API.serializers import MyUserSerializer, ImageSerializer
 from rest_framework.views import APIView
@@ -11,10 +14,35 @@ from rest_framework.views import APIView
 import os
 import boto3
 
+import secrets
+
 # Create statsD obj for cloudwatch
 import statsd
 
 # Create your views here.
+
+class VarifyEmail(APIView):
+
+    def get(self, request, format=None):
+        data = request.GET
+        user_email = data['email']
+        token = data['token']
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('EmailValidTable')
+        response = table.get_item(
+            Key={
+                'EmailAddress': user_email
+            }
+        )
+        item = response['Item']
+        if item == None:
+            return response(status=status.HTTP_403_FORBIDDEN)
+        if item['token'] != token:
+            return response(status=status.HTTP_403_FORBIDDEN)
+        return response(status=status.HTTP_200_OK)
+
+
+
 class UserCreate(APIView):
     """
     List all snippets, or create a new snippet.
@@ -42,9 +70,31 @@ class UserCreate(APIView):
         for _ in user_list:
             if _.get_username() == data['username']:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-        user = MyUser(username=data['username'],first_name=data['first_name'],last_name=data['last_name'],email=data['username'])
+        user = MyUser(username=data['username'],first_name=data['first_name'],last_name=data['last_name'],email=data['username'],is_valid=False)
         user.set_password(data['password'])
         user.save()
+
+        token = secrets.token_urlsafe(20)
+        TTL = int((datetime.datetime.now() + datetime.timedelta(minutes=5)).timestamp())
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('EmailValidTable')
+        table.put_item(
+            Item={
+                'EmailAddress': data['username'],
+                'TTL': TTL,
+                'Token': token,
+            }
+        )
+
+
+        sns_client = boto3.client('sns')
+        sns_client.publish(
+            TopicArn = os.environ['TOPIC_ARN'],
+            Message = 'Email:' + data['username'] + ':Token:' + token,
+        )
+
+
         serializer = MyUserSerializer(user)
         data={
             'id':serializer.data['id'],
@@ -77,6 +127,10 @@ class UserDetail(APIView):
 
         user=request.user
         serializer = MyUserSerializer(user)
+        
+        if not serializer.data['is_valid'] :
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
         data={
             'id':serializer.data['id'],
             'username': serializer.data['email'],
@@ -96,6 +150,10 @@ class UserDetail(APIView):
         
         user = request.user
         data=UserDetail.is_valid_data(request.data.dict())
+        
+        if not MyUserSerializer(user).data['is_valid'] :
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
         if data is not None:
             user.set_password(data['password'])
             user.first_name=data['first_name']
@@ -116,22 +174,21 @@ class UserPic(APIView):
 
     def post(self, request, format=None):
         
+
         counter = statsd.Counter('APICounter')
         counter.increment('UploadPictureCall')
         counter.increment('TotalCall')
 
         user = request.user
         serializer = MyUserSerializer(user)
-
+        if not serializer.data['is_valid'] :
+            return Response(status=status.HTTP_403_FORBIDDEN)
             
         file_obj = request.FILES['profilePic']
         s3 = boto3.client('s3')
-        #     aws_access_key_id='AKIAZAJCF6G3BKJGRSCK',
-        #     aws_secret_access_key='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'
-        # )
+
 
         bucketname=os.environ['S3_Bucket_Name']
-        #bucketname=usedforcsye6225zhenluodeveloptest'
         folder = str(serializer.data['id'])+'/'
         if UserPicture.objects.filter(user_id=serializer.data['id']).count() == 1:
             pic = UserPicture.objects.get(user_id=serializer.data['id'])
@@ -168,6 +225,9 @@ class UserPic(APIView):
         user = request.user
         serializer = MyUserSerializer(user)
 
+        if not serializer.data['is_valid'] :
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
         if UserPicture.objects.filter(user_id=serializer.data['id']).count() == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         pic = UserPicture.objects.get(user_id=serializer.data['id'])
@@ -189,6 +249,9 @@ class UserPic(APIView):
 
         user = request.user
         serializer = MyUserSerializer(user)
+        
+        if not serializer.data['is_valid'] :
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
         if UserPicture.objects.filter(user_id=serializer.data['id']).count() == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
